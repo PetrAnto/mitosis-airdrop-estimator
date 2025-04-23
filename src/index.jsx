ddd285c736734b3bf6c0de2bd42f8e9de222a378
PetrAnto <petrantonft@gmail.com>
2025-04-23 21:22:42 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MitosisAirdropEstimator from './App.jsx';
 import './index.css';
-import MitosisAirdropEstimator from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MitosisAirdropEstimator />
-     <Analytics />
   </React.StrictMode>
 );
